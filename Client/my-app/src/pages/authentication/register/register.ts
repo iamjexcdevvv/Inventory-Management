7fc8bf038f8882c5d70d208b30d34459d89b2f6a
iamjexcdevvv <85010916+iamjexcdevvv@users.noreply.github.com>
2025-06-21 20:34:21 +0800

    implements registration feature

--- Client/my-app/src/pages/authentication/register/register.ts
+++ Client/my-app/src/pages/authentication/register/register.ts
@@ -1,11 +1,37 @@
 import { Component, OnInit } from "@angular/core";
+import {
+    FormControl,
+    FormGroup,
+    ReactiveFormsModule,
+    Validators,
+} from "@angular/forms";
+import { userMustAgreeWithTermsAndCondition } from "../../../shared/custom-validators/user-must-agree.directive";
+import { RouterLink } from "@angular/router";
 
 @Component({
     selector: "register",
     templateUrl: "register.component.html",
+    imports: [ReactiveFormsModule, RouterLink],
 })
 export class RegisterComponent implements OnInit {
+    userRegistrationForm = new FormGroup({
+        firstName: new FormControl("", [Validators.required]),
+        lastName: new FormControl("", [Validators.required]),
+        email: new FormControl("", [Validators.required, Validators.email]),
+        password: new FormControl("", [
+            Validators.required,
+            Validators.minLength(8),
+        ]),
+        isTermsAndRegulationsAccepted: new FormControl(true, [
+            userMustAgreeWithTermsAndCondition()
+        ])
+    });
+
     constructor() {}
+
+    onSubmit() {
+        console.log(this.userRegistrationForm.value);
+    }
 
     ngOnInit() {}
 }
